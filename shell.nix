57f417a5ed1ec457ede5936ca4f768487a5cdd4d
Colin de Roos <8250462+cdfa@users.noreply.github.com>
2021-02-10 16:10:49 +0100

    Add linting

--- shell.nix
+++ shell.nix
@@ -1,38 +1,56 @@
 let
-  sources = import ./nix/sources.nix {};
+  sources = import ./nix/sources.nix { };
 in
-  with import sources.miso {};
-  let
-    ghcPackages = pkgs.haskell.packages.ghc865;
-    reload-script = pkgs.writeShellScriptBin "reload" ''
-      ${pkgs.haskellPackages.ghcid}/bin/ghcid -c '\
-          stack repl\
-          --ghci-options -fno-break-on-exception\
-          app/Main.hs\
-          '\
-          --restart=package.yaml\
-          -T 'Main.main'
-    '';
-    gitIgnore = pkgs.nix-gitignore.gitignoreSourcePure;
-    drv = ghcPackages.callCabal2nix "frugel" (gitIgnore [ ./.gitignore ] ./.) {
-      miso = miso-jsaddle; /* Overrides miso dependency defined in package.yaml */
+with import sources.miso { };
+let
+  unstablePkgs = import sources.nixpkgs-unstable { };
+  stablePkgs = import sources.nixpkgs { };
+  ghcPackages = pkgs.haskell.packages.ghc865;
+  gitIgnore = pkgs.nix-gitignore.gitignoreSourcePure;
+  drv = ghcPackages.callCabal2nix "frugel" (gitIgnore [ ./.gitignore ] ./.) {
+    miso = miso-jsaddle; /* Overrides miso dependency defined in package.yaml */
+  };
+  reload-script = pkgs.writeShellScriptBin "reload" ''
+    ${pkgs.haskellPackages.ghcid}/bin/ghcid -c '\
+        stack repl\
+        --ghci-options -fno-break-on-exception\
+        app/Main.hs\
+        '\
+        --restart=package.yaml\
+        -T 'Main.main'
+  '';
+  floskell = unstablePkgs.haskellPackages.floskell;
+  nix-pre-commit-hooks = import sources."pre-commit-hooks.nix";
+  pre-commit-check = nix-pre-commit-hooks.run {
+    src = ./.;
+    hooks = {
+      nixpkgs-fmt.enable = true;
+      nix-linter.enable = true;
+      hlint.enable = true;
+      floskell =
+        {
+          enable = true;
+          name = "floskell";
+          description = "A flexible Haskell source code pretty printer.";
+          entry = "${floskell}/bin/floskell";
+          files = "\\.l?hs$";
+        };
     };
-  in
-    drv.env.overrideAttrs (
-      old: {
-        buildInputs =
-          let
-            unstablePkgs = import sources.nixpkgs-unstable {};
-            stablePkgs = import sources.nixpkgs {};
-          in
-            old.buildInputs ++ [
-              reload-script
-              stablePkgs.hlint
-              stablePkgs.haskellPackages.apply-refact
-              unstablePkgs.haskellPackages.floskell
-              stablePkgs.ghcid
-              stablePkgs.stack
-              pkgs.haskell.packages.ghcjs.ghc
-            ];
-      }
-    )
+  };
+in
+drv.env.overrideAttrs (
+  old: {
+    buildInputs = old.buildInputs ++ [
+      reload-script
+      stablePkgs.hlint
+      stablePkgs.haskellPackages.apply-refact
+      floskell
+      stablePkgs.ghcid
+      stablePkgs.stack
+      pkgs.haskell.packages.ghcjs.ghc
+    ];
+    shellHook = ''
+      ${pre-commit-check.shellHook}
+    '';
+  }
+)
