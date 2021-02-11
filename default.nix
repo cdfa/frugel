1d74ba6d48c5c49db38fe85ba3737aaeebab99e9
Colin de Roos <8250462+cdfa@users.noreply.github.com>
2021-02-11 14:44:54 +0100

    Fix building of base-noprelude

--- default.nix
+++ default.nix
@@ -1,8 +1,11 @@
 let
-  sources = import ./nix/sources.nix {};
+  sources = import ./nix/sources.nix { };
 in
-  with import sources.miso {};
-  let
-    gitIgnore = pkgs.nix-gitignore.gitignoreSourcePure;
-  in
-    pkgs.haskell.packages.ghcjs.callCabal2nix "frugel" (gitIgnore [ ./.gitignore ] ./.) {}
+with import sources.miso { };
+let
+  gitIgnore = pkgs.nix-gitignore.gitignoreSourcePure;
+  base-noprelude-ghcjs = pkgs.haskell.packages.ghcjs.callCabal2nix "base-noprelude-ghcjs" sources.base-noprelude { };
+in
+pkgs.haskell.packages.ghcjs.callCabal2nix "frugel" (gitIgnore [ ./.gitignore ] ./.) {
+  base-noprelude = base-noprelude-ghcjs;
+}
