c71a46e13a86ab58854de51492ac67d33a2deded
Colin de Roos <8250462+cdfa@users.noreply.github.com>
2021-05-12 17:22:14 +0200

    Set up testing

--- base.nix
+++ base.nix
@@ -1,17 +1,20 @@
 { sources, pkgs, ghc }:
 let
-  stablePkgs = import sources.nixpkgs { };
+  stablePkgs = import sources.nixpkgs {};
 
   gitIgnore = stablePkgs.nix-gitignore.gitignoreSourcePure;
 
   packageSourceOverrides = pkgs.lib.mapAttrs (
     name: version:
-      pkgs.haskell.packages.${ghc}.callHackage name version { }
+      pkgs.haskell.packages.${ghc}.callHackage name version {}
   );
   overrides = packageSourceOverrides {
     megaparsec = "9.0.1";
     relude = "0.7.0.0";
     prettyprinter = "1.7.0";
+    optics = "0.3";
+    it-has = "0.2.0.0";
+    genvalidity-sydtest = "0.0.0.0";
   };
 in
 pkgs.haskell.packages.${ghc}.callCabal2nix "frugel" (gitIgnore [ ./.gitignore ] ./.)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,7 @@
+import           Test.Syd
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = sydTest $ do
+    describe "addition" $ do
+        it "adds 3 to 5 to result in 8" $ 3 + 5 `shouldBe` 8
+        it "adds 4 to 7 to result in 11" $ 4 + 7 `shouldBe` 11
