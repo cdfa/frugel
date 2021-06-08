5dff878c59476c3f003872964749e76dd561b8c4
Colin de Roos <8250462+cdfa@users.noreply.github.com>
2021-06-08 15:28:11 +0200

    Parse nested construction sites

--- src/Frugel/Action.hs
+++ src/Frugel/Action.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE UndecidableInstances #-}
 
@@ -6,6 +8,7 @@
 
 module Frugel.Action where
 
+import           Control.Monad.Except
 import           Control.Zipper.Seq                      hiding ( insert )
 import qualified Control.Zipper.Seq                      as SeqZipper
 
@@ -13,7 +16,8 @@ import qualified Data.Sequence                           as Seq
 import qualified Data.Set                                as Set
 
 import           Frugel.Decomposition
-                 hiding ( ModificationStatus(..) )
+                 hiding ( ModificationStatus(Success) )
+import qualified Frugel.Decomposition                    as Decomposition
 import           Frugel.DisplayProjection
 import           Frugel.Error
 import           Frugel.Model
@@ -181,6 +185,8 @@ textVariations
         = (if is _NodeCstrSite node then cons item <$> variations else mempty)
         <> (mappend <$> textVariations (decompose node) <*> variations)
 
+type CstrSiteZipper = SeqZipper (Either Char Node)
+
 zipperAtCursor :: (CstrSiteZipper -> Maybe CstrSiteZipper)
     -> Int
     -> Program
@@ -192,3 +198,75 @@ zipperAtCursor f
                         _CstrSite
                         (rezip <.> f <=< unzipTo cstrSiteOffset)
                         materials)
+
+step :: MonadState DecompositionState m => m ()
+step = do
+    textOffset <- use #textOffset
+    -- c <- guse #cstrSiteOffset
+    -- traceM ("step t: " <> show textOffset <> " c: " <> show c)
+    when (textOffset /= -1) (#textOffset -= 1)
+    when (textOffset > 0) (#cstrSiteOffset += 1)
+
+modifyNodeAt :: forall m p.
+    (MonadError InternalError m, Decomposable p, SetCstrSite p)
+    => (Int -> CstrSite -> m CstrSite)
+    -> Int
+    -> p
+    -> m p
+modifyNodeAt f cursorOffset program
+    = runModification >>= \(newProgram, decompositionState) -> if
+        | view #textOffset decompositionState
+            > 0 -> throwError $ DecompositionFailed cursorOffset
+        | Todo <- view #modificationStatus decompositionState ->
+            throwError $ ASTModificationNotPerformed cursorOffset
+        | otherwise -> pure newProgram
+  where
+    runModification
+        = mapNode program `runStateT` initialDecompositionState cursorOffset
+    mapChar c = c <$ step -- trace (show c) step
+    mapNode :: forall n.
+        (Decomposable n, SetCstrSite n)
+        => n
+        -> DecompositionMonad m n
+    -- mapNode n = do
+        -- t <- guse #textOffset
+        -- traceM ("pre " <> take 20 (show n) <> " " <> show t)
+    mapNode n
+        = ifM (guses #textOffset (< 0)) (pure n) -- node is located after cursor
+        $ do
+            #cstrSiteOffset
+                += 1 -- At the moment, the construction site offset passed to `f` is immediately after a node where applying `f` failed. This is not a good default.
+            withLocal #cstrSiteOffset 0 $ do
+                newNode <- mapMComponents mapChar mapNode n
+                -- traceM ("post " <> take 20 (show n))
+                modificationStatus <- guse #modificationStatus
+                ifM
+                    (guses #textOffset (<= 0)
+                     <&> (&& modificationStatus == Todo))
+                    (catchError
+                         (setCstrSite <$> transform n
+                          ?? n
+                          <* assign #modificationStatus Decomposition.Success)
+                     $ const (pure n))
+                    (pure
+                     $ fromMaybe
+                         newNode
+                         (guard (modificationStatus == Decomposition.Success)
+                          >> hasParser
+                          >>= \parser -> getLast
+                          . foldMap
+                              (Last
+                               . rightToMaybe
+                               . runParser (parser <* eof) "")
+                          . textVariations
+                          $ decompose newNode))
+    -- transform :: (Decomposable n) => n -> StateT DecompositionState m' CstrSite
+    transform n = do
+        cstrSiteOffset <- use #cstrSiteOffset
+        lift $ case conservativelyDecompose cstrSiteOffset n of
+            Just (cstrSiteOffset', cstrSite)
+                | cstrSiteOffset == 0
+                    || cstrSiteOffset == length (toList $ decompose n) ->
+                    catchError (f cstrSiteOffset' cstrSite)
+                    $ const (f cstrSiteOffset $ decompose n)
+            _ -> f cstrSiteOffset $ decompose n
--- src/Frugel/Internal/DecompositionState.hs
+++ src/Frugel/Internal/DecompositionState.hs
@@ -16,12 +16,13 @@ module Frugel.Internal.DecompositionState
     , initialDecompositionState
     , _Todo
     , _Success
+    , _Reparsed
     ) where
 
 import           Optics
 
-data ModificationStatus = Todo | Success
-    deriving ( Show )
+data ModificationStatus = Todo | Success | Reparsed
+    deriving ( Show, Eq )
 
 -- A text offset of -1 is used for representing that we are done with parsing
 -- because 0 could also mean we are at the start of a node that still needs to be decomposed
@@ -30,7 +31,7 @@ data DecompositionState
                          , cstrSiteOffset     :: Int
                          , modificationStatus :: ModificationStatus
                          }
-    deriving ( Show )
+    deriving ( Show, Eq )
 
 type DecompositionMonad m = StateT DecompositionState m
 
