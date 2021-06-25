290f84edf5f0407c7f6d39469b9d0be9464bce38
Colin de Roos <8250462+cdfa@users.noreply.github.com>
2021-06-25 12:29:28 +0200

    Minimum height for construction sites

--- app/Frugel/View/Elements.hs
+++ app/Frugel/View/Elements.hs
@@ -37,7 +37,9 @@ paddingStyle
 inConstructionStyles :: [Attribute action]
 inConstructionStyles
     = [ style_
-        $ "background-color" =: "hsl(48, 100%, 85%)" <> "min-width" =: "0.6em"
+        $ "background-color" =: "hsl(48, 100%, 85%)"
+        <> "min-width" =: "0.6em"
+        <> "min-height" =: "2em"
       , paddingStyle
       ]
 
