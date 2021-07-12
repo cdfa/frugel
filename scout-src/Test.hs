f1a9bc85e7c3e5f6825e93c02f7f63bb1e83dce3
Colin de Roos <8250462+cdfa@users.noreply.github.com>
2021-07-12 13:14:53 +0200

    Use Reader monad
    in preparation for error handling

--- scout-src/Test.hs
+++ scout-src/Test.hs
@@ -9,7 +9,7 @@ import Data.Map
 data LamN
     = VarN Name
     | AppN LamN LamN
-    | AbsN (Maybe (Hidden (LamN -> LamN))) Name LamN
+    | AbsN (Maybe (Hidden (LamN -> EvaluationMonad LamN))) Name LamN
     | LitN Integer
     | PLusN LamN LamN
     deriving ( Eq, Show )
@@ -18,19 +18,30 @@ type Name = String
 
 type EvaluationEnv = Map Name LamN
 
+type EvaluationMonad = Reader EvaluationEnv
+
 -- Just returning v in case it's not in the environment is cool because it kills 2 powerful birds with 1 stone: it adds tolerance for binding errors and it makes it possible to print partially applied functions
-eval :: EvaluationEnv -> LamN -> LamN
-eval env e@(VarN n) = fromMaybe e $ lookup n env
-eval env (AppN f x) = let ex = eval env x in case eval env f of
-    AbsN (Just (Hidden liftedFunction)) _ _ -> liftedFunction ex
-    ef -> AppN ef ex
-eval env (AbsN _ n e)
-    = AbsN (Just . Hidden $ \x -> eval (insert n x env) e) n
-    $ eval (delete n env) e -- delete n from environment, because otherwise a shadowed variable may be used
-eval _ i@(LitN _) = i
-eval env (PLusN x y) = case (eval env x, eval env y) of
-    (LitN a, LitN b) -> LitN (a + b)
-    (ex, ey) -> PLusN ex ey
+eval :: LamN -> EvaluationMonad LamN
+eval e@(VarN n) = asks $ fromMaybe e . lookup n
+eval (AppN f x) = do
+    ef <- eval f
+    ex <- eval x
+    case ef of
+        AbsN (Just (Hidden liftedFunction)) _ _ -> liftedFunction ex
+        _ -> return $ AppN ef ex
+eval (AbsN _ n e) = ask >>= \env ->
+    AbsN (Just . Hidden $ \x -> local (const $ insert n x env) $ eval e) n
+    <$> local (delete n) (eval e)  -- delete n from environment, because otherwise a shadowed variable may be used
+eval i@(LitN _) = return i
+eval (PLusN x y) = do
+    ex <- eval x
+    ey <- eval y
+    return $ case (ex, ey) of
+        (LitN a, LitN b) -> LitN (a + b)
+        _ -> PLusN ex ey
+
+runEval :: LamN -> LamN
+runEval = usingReader mempty . eval
 
 k :: LamN
 k = AbsN Nothing "x" $ AbsN Nothing "_" $ VarN "x"
@@ -42,26 +53,25 @@ s
 
 -- Simple test
 test :: LamN
-test = eval mempty (AppN (AppN (AppN s k) k) (LitN 3))
+test = runEval (AppN (AppN (AppN s k) k) (LitN 3))
 
 -- LiftedFunction is lazy
 test2 :: LamN
-test2 = eval mempty (AppN (AppN k (LitN 42)) (error "should not be evaluated"))
+test2 = runEval (AppN (AppN k (LitN 42)) (error "should not be evaluated"))
 
 -- LiftedFunction gets as far as possible with partial application
 test3 :: LamN
-test3 = eval mempty (AppN k $ LitN 5)
+test3 = runEval (AppN k $ LitN 5)
 
 -- Call-by-need
 test4 :: LamN
 test4
-    = eval mempty
-           (AppN (AbsN Nothing "x" $ PLusN (VarN "x") (VarN "x"))
-            $ trace "test"
-            $ LitN 1)
+    = runEval (AppN (AbsN Nothing "x" $ PLusN (VarN "x") (VarN "x"))
+               $ trace "test"
+               $ LitN 1)
 
 shadowingTest :: LamN
 shadowingTest
-    = eval mempty
+    = runEval
     $ AppN (AbsN Nothing "x" $ AbsN Nothing "x" $ VarN "x")
            (error "should not be evaluated")
