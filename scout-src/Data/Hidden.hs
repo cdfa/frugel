481500339ea72a5d1a9a0b556d57f943be932ded
Colin de Roos <8250462+cdfa@users.noreply.github.com>
2021-09-10 10:51:02 +0200

    Refine purpose of Hidden

--- scout-src/Data/Hidden.hs
+++ scout-src/Data/Hidden.hs
@@ -8,17 +8,17 @@ import Data.GenValidity
 import Text.Show
 
 -- | A type that is not interacted with in any other way than through the constructor.
--- | The defined instances may break laws, but are well-defined (no undefined should show up).
--- | Instances of types that include this type are only legal for values that do not include a Hidden value
+-- | This may be useful for derived data would otherwise prevent the definition of some instances for a data structure containing the data.
+-- | The defined instances may be ill-defined or break laws.
+-- | It is the programmers responsibility to ensure that functions constrained on these instances are not applied to values containing hidden data.
 newtype Hidden a = Hidden a
     deriving ( Typeable )
 
 instance Eq (Hidden a) where
     _ == _ = True -- What is hidden should not matter
 
--- Breaks law that it produces a valid Haskell expression
 instance Show (Hidden a) where
-    show _ = "Hidden"
+    show _ = undefined
 
 instance Ord (Hidden a) where
     _ <= _ = True
