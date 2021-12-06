c113327f4bd39311220ccaa136d373e0fba0f497
Colin de Roos <8250462+cdfa@users.noreply.github.com>
2021-12-06 16:27:00 +0100

    Document issues with cross-compilation

--- default.nix
+++ default.nix
@@ -5,23 +5,28 @@ let
   haskellNix = import sources.haskellNix { };
   pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
   hsPkgs = import ./base.nix { inherit pkgs ghc; };
-  frugel-js = hsPkgs.projectCross.ghcjs.hsPkgs.frugel.components.exes.frugel-exe;
+  frugel-cross = { platform }: hsPkgs.projectCross.${platform}.hsPkgs.frugel.components.exes.frugel-exe;
 in
-pkgs.stdenv.mkDerivation {
-  name = "frugel-web";
-  src = pkgs.haskell-nix.haskellLib.cleanGit {
-    name = "frugel";
-    src = ./.;
-    subDir = "www";
+{
+  frugel-web = pkgs.stdenv.mkDerivation {
+    name = "frugel-web";
+    src = pkgs.haskell-nix.haskellLib.cleanGit {
+      name = "frugel";
+      src = ./.;
+      subDir = "www";
+    };
+    buildInputs = [ pkgs.closurecompiler ];
+    installPhase = ''
+      mkdir -p $out
+      find . \( -name '*.html' -o -name '*.css' \) -exec cp {} $out \;
+      FRUGEL_WEB=${frugel-cross { platform = "ghcjs"; }}/bin/frugel-exe.jsexe
+      closure-compiler \
+        $FRUGEL_WEB/all.js --externs $FRUGEL_WEB/all.js.externs \
+        -O ADVANCED --jscomp_off=checkVars -W QUIET \
+        --js_output_file $out/all.min.js
+    '';
   };
-  buildInputs = [ pkgs.closurecompiler ];
-  installPhase = ''
-    mkdir -p $out
-    find . \( -name '*.html' -o -name '*.css' \) -exec cp {} $out \;
-    FRUGEL_WEB=${frugel-js}/bin/frugel-exe.jsexe
-    closure-compiler \
-      $FRUGEL_WEB/all.js --externs $FRUGEL_WEB/all.js.externs \
-      -O ADVANCED --jscomp_off=checkVars -W QUIET \
-      --js_output_file $out/all.min.js
-  '';
+  # frugel-aarch64-darwin = frugel-cross { platform = "aarch64-darwin"; }; Not working due to https://github.com/NixOS/nixpkgs/issues/49526
+  # frugel-gnu64 = frugel-cross { platform = "gnu64"; }; Not working due to https://github.com/NixOS/nixpkgs/issues/56493 ?
+  # frugel-mingwW64 = frugel-cross { platform = "mingwW64"; }; Not working due to https://github.com/NixOS/nixpkgs/issues/36200
 }
