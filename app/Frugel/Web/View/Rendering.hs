8cab27ee93639a108ccf124158c2ac7a364a24f7
Colin de Roos <8250462+cdfa@users.noreply.github.com>
2021-12-16 12:33:12 +0100

    Fix empty construction site rendering

--- app/Frugel/Web/View/Rendering.hs
+++ app/Frugel/Web/View/Rendering.hs
@@ -60,7 +60,7 @@ renderDocStream
 
 textTreeForm :: SimpleDocTree Annotation -> [DocTextTree]
 textTreeForm = \case
-    STEmpty -> one $ TextLeaf ""
+    STEmpty -> []
     STChar '\n' -> one LineLeaf -- Normally, there would be no newlines in STChar, but these are explicitly inserted by renderCstrSite' to prevent insertion of extra whitespace when pretty printing construction sites which are `nest`ed
     STChar c -> one . TextLeaf $ one c
     STText _ t -> one $ TextLeaf t
